6dd9e4f003093a3d97a7fa0941f84ed1f7d8fa15
Gantay <Gantay@proton.me>
2024-10-11 19:44:35 +0300

    d

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,6 +1,39 @@
 package lexer
 
 import (
-	"monkey/token"
+	"github.com/Gantay/Interpreter/token"
 	"testing"
 )
+
+func TestNextToken(t *testing.T) {
+
+	input := `=+(){},;`
+
+	tests := []struct {
+		expectedType    token.TokenType
+		expectedLiteral string
+	}{
+		{token.ASSING, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	l := New(input)
+
+	for i, tt := range tests {
+		tok := l.NextToken()
+
+		if tok.Type != tt.expectedType {
+			t.Fatalf("test[%d] - tokentype worng. expected=%q, got=%q", i, tt.expectedType, tok.Type)
+		}
+		if tok.Literal != tt.expectedLiteral {
+			t.Fatalf("test[%d] - literal worng. expected=%q, got=%q", i, tt.expectedLiteral, tok.Literal)
+		}
+	}
+}
--- token/token.go
+++ token/token.go
@@ -3,7 +3,7 @@ package token
 type TokenType string
 
 type Token struct {
-	Token   TokenType
+	Type    TokenType
 	Literal string
 }
 
@@ -22,6 +22,11 @@ const (
 	//Delimiters
 	COMMA     = ","
 	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
 
 	//Keywords
 	FUNCTION = "FUNCTION"
