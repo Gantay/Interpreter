e3ea438e9ab5a1bb81dd6867595c5ce17a841cf7
Gantay <Gantay@proton.me>
2024-10-18 00:22:31 +0300

    P:30

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 
 	user, err := user.Current()
 	if err != nil {
-		panic(err)
+		panic("Who are you?")
 	}
 
 	fmt.Printf("Hello %s! This is Monkey programming language!\n", user.Username)
