d7c2a988fda33c8621935cf768ac8377fb8f4cab
Gantay <Gantay@proton.me>
2024-10-17 13:26:02 +0300

    chapter 1 completed

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"github.com/Gantay/Interpreter/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is Monkey programming language!\n", user.Username)
+
+	fmt.Printf("Fell free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
